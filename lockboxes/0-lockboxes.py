20b8030ba7c119b20291c95c09e71e34862a8f6d
Rbrogniez <5013@holbertonschool.com>
2023-08-29 15:16:31 +0200

    doc

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
+    """
+    Determine if all locked boxes can be opened using provided keys.
+
+    Args:
+        boxes (list of list): A list of lists where each sub-list represents the keys in a box.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)  # Total number of boxes
     unlocked = [False] * n  # List to track whether each box is unlocked
     unlocked[0] = True  # The first box is unlocked by default
@@ -14,5 +23,4 @@ def canUnlockAll(boxes):
                 stack.append(key)
 
     return all(unlocked)
-
 
