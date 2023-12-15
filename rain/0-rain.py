3ea7353dd71f235075dd6f92fc89fce08b917982
Rbrogniez <5013@holbertonschool.com>
2023-12-15 21:12:44 +0100

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python3
+"""
+Module to calculate the amount of rainwater retained between walls.
+"""
+
 def rain(walls):
-    if not walls:
-        return 0
+    """
+    Calculate the amount of rainwater retained between walls.
+
+    Args:
+        walls (List[int]): List of non-negative integers representing wall heights.
+
+    Returns:
+        int: Total amount of rainwater retained.
+
+    Raises:
+        ValueError: If the input list is empty or has less than 3 elements.
+    """
+    if not walls or len(walls) < 3:
+        raise ValueError("Input list must contain at least 3 elements.")
 
     n = len(walls)
-    left_max, right_max = [0] * n, [0] * n
-    left_max[0] = walls[0]#!/usr/bin/python3
-    right_max[n - 1] = walls[n - 1]
+    left_max = [0] * n
+    right_max = [0] * n
 
-    # Calculate the maximum height from the left for each position
+    left_max[0] = walls[0]
     for i in range(1, n):
         left_max[i] = max(left_max[i - 1], walls[i])
 
-    # Calculate the maximum height from the right for each position
+    right_max[n - 1] = walls[n - 1]
     for i in range(n - 2, -1, -1):
         right_max[i] = max(right_max[i + 1], walls[i])
 
     total_water = 0
-
-    # Calculate the amount of water that can be retained at each position
-    for i in range(n):
-        water_at_position = min(left_max[i], right_max[i]) - walls[i]
-        total_water += max(water_at_position, 0)
+    for i in range(1, n - 1):
+        min_height = min(left_max[i], right_max[i])
+        if min_height > walls[i]:
+            total_water += min_height - walls[i]
 
     return total_water
