3a9154a495484a37b17211a689f9931328e6028e
Rbrogniez <5013@holbertonschool.com>
2023-12-15 21:16:56 +0100

    new case

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -14,46 +14,11 @@ def rain(walls):
         int: Total amount of rainwater retained.
 
     Raises:
-        ValueError: If the input list is empty or has less than 3 elements.
-    """
-    if not walls or len(walls) < 3:
-        raise ValueError("Input list must contain at least 3 elements.")
-
-    n = len(walls)
-    left_max = [0] * n
-    right_max = [0] * n
-
-    left_max[0] = walls[0]
-    for i in range(1, n):
-        left_max[i] = max(left_max[i - 1], walls[i])
-
-    right_max[n - 1] = walls[n - 1]
-    for i in range(n - 2, -1, -1):
-        right_max[i] = max(right_max[i + 1], walls[i])
-
-    total_water = 0
-    for i in range(1, n - 1):
-        min_height = min(left_max[i], right_max[i])
-        if min_height > walls[i]:
-            total_water += min_height - walls[i]
-#!/usr/bin/python3
-"""
-Module to calculate the amount of rainwater retained between walls.
-"""
-
-def rain(walls):
-    """
-    Calculate the amount of rainwater retained between walls.
-
-    Args:
-        walls (List[int]): List of non-negative integers representing wall heights.
-
-    Returns:
-        int: Total amount of rainwater retained.
-
-    Raises:
         ValueError: If the input list has less than 3 elements.
     """
+    if not walls:
+        return 0
+
     if len(walls) < 3:
         raise ValueError("Input list must contain at least 3 elements.")
 
@@ -76,3 +41,4 @@ def rain(walls):
             total_water += min_height - walls[i]
 
     return total_water
+
