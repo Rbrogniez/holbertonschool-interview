8bbd3cf2b3ba0b8acd0da1679c5a5bd7ddbc0a35
Rbrogniez <105486378+Rbrogniez@users.noreply.github.com>
2024-01-19 21:17:11 +0100

    Update search_algos.h

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -1,10 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <stdlib.h>
 #include <stdio.h>
-
-int advanced_binary_recursive(int *array, size_t start, size_t end, int value);
 int advanced_binary(int *array, size_t size, int value);
 
-#endif /*SEARCH_ALGOS_H*/
+#endif /* SEARCH_ALGOS_H */
