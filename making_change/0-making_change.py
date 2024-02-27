e5a5ff34111a1e164c8adcf71c9a9152265d9958
Rbrogniez <5013@holbertonschool.com>
2024-02-27 09:31:02 +0100

    making change

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -8,18 +8,15 @@ def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
     """
-    if total < 0:
-        return -1
-    
+    if total <= 0:
+        return 0
     coins.sort(reverse=True)
-    numb_coins = 0
-
+    num_coins = 0
     for coin in coins:
-        if total >= coin:
-            numb_coins += total // coin
-            total %= coin
-
-    if total == 0:
-        return numb_coins
-    else:
+        if total <= 0:
+            break
+        num_coins += total // coin
+        total = total % coin
+    if total != 0:
         return -1
+    return num_coins
