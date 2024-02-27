b6bd21f24a5a12717dae908baf0404267ea5a838
Rbrogniez <5013@holbertonschool.com>
2024-02-27 09:24:46 +0100

    adding if

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -8,6 +8,9 @@ def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
     """
+    if total < 0:
+        return -1
+    
     coins.sort(reverse=True)
     numb_coins = 0
 
