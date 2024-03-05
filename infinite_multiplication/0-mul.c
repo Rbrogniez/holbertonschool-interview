d4e69deade172287d6b5637da1985b34370af04a
Rbrogniez <5013@holbertonschool.com>
2024-03-05 09:34:39 +0100

    infinite_multiplication

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,93 +1,159 @@
 #include "holberton.h"
-#include <stdlib.h>
 
 /**
- * error_and_exit - Prints "Error" and exits with status 98.
+ * main - multiplies two positive numbers
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 in case of success, 98 in case of failure, 1 if argc != 3
  */
-void error_and_exit(void)
+int main(int argc, char **argv)
 {
-    char *error_msg = "Error\n";
+	int len1, len2, len_result, *result;
 
-    while (*error_msg)
-    {
-        _putchar(*error_msg);
-        error_msg++;
-    }
+	if (argc != 3 || !verify_arg(argv[1]) || !verify_arg(argv[2]))
+	{
+		_puts("Error\n");
+		exit(98);
+	}
+	len1 = _strlen(argv[1]);
+	len2 = _strlen(argv[2]);
+	len_result = len1 + len2;
 
-    exit(98);
+	result = malloc(len_result * sizeof(int));
+
+	if (!result)
+		return (EXIT_FAILURE);
+
+	initialize_array(result, len_result);
+	multiply_numbers(argv[1], argv[2], result, len1, len2);
+	print_result(result, len_result);
+
+	free(result);
+	return (EXIT_SUCCESS);
 }
 
 /**
- * is_digit - Checks if a string contains only digits.
- * @str: The input string.
- * Return: 1 if all characters are digits, 0 otherwise.
+ * _puts - prints a string
+ *
+ * @s: string to print
+ *
+ * Return: the length of the string
  */
-int is_digit(char *str)
+int _puts(char *s)
 {
-    while (*str)
-    {
-        if (*str < '0' || *str > '9')
-            return 0;
-        str++;
-    }
-    return 1;
+	int length = 0;
+
+	for (; *s; s++)
+		length += _putchar(*s);
+
+	return (length);
 }
 
 /**
- * multiply - Multiplies two positive numbers and prints the result.
- * @num1: The first number.
- * @num2: The second number.
+ * _strlen - Finds the length of a string
+ *
+ * @s: string to find the length
+ *
+ * Return: the length of the string
  */
-void multiply(char *num1, char *num2)
+int _strlen(char *s)
 {
-  int len1 = 0, len2 = 0;
+	int i = 0;
 
-    while (num1[len1])
-        len1++;
-    while (num2[len2])
-        len2++;
+	while (s[i] != '\0')
+		i++;
 
-    int result_len = len1 + len2;
-    int *result = malloc(sizeof(int) * result_len);
-    if (result == NULL)
-        error_and_exit();
-
-    /* Initialize the result array to 0 */
-    for (int i = 0; i < result_len; i++)
-        result[i] = 0;
-
-    /* Multiply each digit and add to the result */
-    for (int i = len1 - 1; i >= 0; i--)
-    {
-        int carry = 0;
-        for (int j = len2 - 1; j >= 0; j--)
-        {
-            int mul = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
-            result[i + j + 1] = mul % 10;
-            carry = mul / 10;
-        }
-        result[i] += carry;
-    }
-
-    /* Print the result using _putchar */
-    int start = 0;
-    while (start < result_len - 1 && result[start] == 0)
-        start++;
-
-    for (int i = start; i < result_len; i++)
-        _putchar(result[i] + '0');
-
-    _putchar('\n');
-
-    free(result);
+	return (i);
 }
 
-int main(int argc, char *argv[])
+/**
+ * verify_arg - Verifies if the argument is a number or not
+ *
+ * @num: string to verify
+ *
+ * Return: 1 if the argument is a number, 0 otherwise
+ */
+char verify_arg(char *num)
 {
-    if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-        error_and_exit();
+	int i;
 
-    multiply(argv[1], argv[2]);
+	for (i = 0; num[i] != '\0'; i++)
+	{
+		if (num[i] < '0' || num[i] > '9')
+			return (0);
+	}
 
-    return 0;
+	return (1);
+}
+
+/**
+ * multiply_numbers - Multiplies two numbers
+ *
+ * @num1: first number as a string
+ * @num2: second number as a string
+ * @result: array of int to contain the product
+ * @len1: length of the first number
+ * @len2: length of the second number
+ */
+void multiply_numbers(char *num1, char *num2, int *result, int len1, int len2)
+{
+	int i, j, product, pos1, pos2, digit1, digit2;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		digit1 = num1[i] - '0';
+
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			digit2 = num2[j] - '0';
+			product = digit1 * digit2;
+
+			pos1 = i + j;
+			pos2 = i + j + 1;
+
+			product += result[pos2];
+
+			result[pos1] += product / 10;
+			result[pos2] = product % 10;
+		}
+	}
+}
+
+/**
+ * initialize_array - Initializes an array with 0
+ *
+ * @array: array to initialize
+ * @size: size of the array
+ */
+void initialize_array(int *array, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		array[i] = 0;
+}
+
+/**
+ * print_result - Prints the result of the multiplication
+ *
+ * @array: array containing the result
+ * @size: size of the array
+ */
+void print_result(int *array, int size)
+{
+	int i = 0;
+
+	while (i < size && array[i] == 0)
+		i++;
+
+	if (i == size)
+		_putchar('0');
+	else
+	{
+		for (; i < size; i++)
+			_putchar(array[i] + '0');
+	}
+	_putchar('\n');
 }
--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -1,10 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/* Function prototypes */
+#include <stdlib.h>
+
 int _putchar(char c);
-void error_and_exit(void);
-int is_digit(char *str);
-void multiply(char *num1, char *num2);
+int _puts(char *s);
+char verify_arg(char *num);
+int _strlen(char *s);
+void multiply_numbers(char *num1, char *num2, int *result, int len1, int len2);
+void initialize_array(int *array, int size);
+void print_result(int *array, int size);
 
 #endif /* HOLBERTON_H */
