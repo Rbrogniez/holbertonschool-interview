9197392595ae6179ee14832ff86d66f1321c309a
Rbrogniez <5013@holbertonschool.com>
2024-03-05 09:44:18 +0100

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,23 +1,44 @@
 #!/usr/bin/python3
-"""
-    Determine if all locked boxes can be opened using provided keys.
-"""
+"""Module checking if the boxes can all be unlocked"""
+
 
 def canUnlockAll(boxes):
     """
-    Determine if all locked boxes can be opened using provided keys.
+    Determines if all the boxes in a list of boxes can be unlocked.
+
+    Args:
+    - boxes (list): boxes and their keys
+
+    Returns:
+    - bool: True if all boxes can be unlocked, False otherwise
+    """
+    open_boxes = {0: True}
+    for i in range(len(boxes)):
+        if i not in open_boxes.keys():
+            open_boxes.update({i: False})
+        open_boxes = unlockBoxes(boxes, open_boxes, i)
+    return len(set(open_boxes.values())) == 1
+
+
+def unlockBoxes(boxes, open_boxes, index):
+    """
+    Recursively unlocks boxes in a list of boxes.
+
+    Args:
+    - boxes (list): all boxes and their keys
+    - open_boxes (dict): state of each box
+    - index (int): index of the box to be opened
+
+    Returns:
+    - dict: updated state of each box
     """
 
-    n = len(boxes)  # Total number of boxes
-    unlocked = [False] * n  # List to track whether each box is unlocked
-    unlocked[0] = True  # The first box is unlocked by default
-    stack = [0]  # Start with the first box
-
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if key < n and not unlocked[key]:
-                unlocked[key] = True
-                stack.append(key)
-
-    return all(unlocked)
+    if open_boxes.get(index):
+        for key in boxes[index]:
+            is_opened = open_boxes.get(key)
+            if key < index and not is_opened:
+                open_boxes.update({key: True})
+                open_boxes = unlockBoxes(boxes, open_boxes, key)
+            elif not is_opened:
+                open_boxes.update({key: True})
+    return open_boxes
