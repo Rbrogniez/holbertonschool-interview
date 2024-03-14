8bcc85089e1428ca5687879bc7e38e21a55d6ab6
Rbrogniez <5013@holbertonschool.com>
2024-03-14 11:51:21 +0100

    betty style

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -3,59 +3,60 @@
 
 /* Helper function to get the maximum value in the array */
 int getMax(int *array, size_t size) {
-    int max = array[0];
-    size_t i;
-    for (i = 1; i < size; i++) {
-        if (array[i] > max) {
-            max = array[i];
-        }
-    }
-    return max;
+	int max = array[0];
+	size_t i;
+
+	for (i = 1; i < size; i++) {
+		if (array[i] > max) {
+			max = array[i];
+		}
+	}
+	return max;
 }
 
 /* Helper function to perform counting sort */
 void countSort(int *array, size_t size, int exp) {
-    int *output = malloc(size * sizeof(int));
-    size_t i;
-    int count[10] = {0}; /* Declaration moved up */
+	int *output = malloc(size * sizeof(int));
+	int count[10] = {0}; /* Declaration moved up */
+	size_t i;
 
-    if (output == NULL) {
-        /* Handle allocation failure */
-        return;
-    }
+	if (output == NULL) {
+		/* Handle allocation failure */
+		return;
+	}
 
-    for (i = 0; i < size; i++) {
-        count[(array[i] / exp) % 10]++;
-    }
+	for (i = 0; i < size; i++) {
+		count[(array[i] / exp) % 10]++;
+	}
 
-    for (i = 1; i < 10; i++) {
-        count[i] += count[i - 1];
-    }
+	for (i = 1; i < 10; i++) {
+		count[i] += count[i - 1];
+	}
 
-    for (i = size; i > 0; i--) {
-        output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
-        count[(array[i - 1] / exp) % 10]--;
-    }
+	for (i = size; i > 0; i--) {
+		output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
+		count[(array[i - 1] / exp) % 10]--;
+	}
 
-    for (i = 0; i < size; i++) {
-        array[i] = output[i];
-    }
+	for (i = 0; i < size; i++) {
+		array[i] = output[i];
+	}
 
-    free(output);
+	free(output);
 }
 
 /* Main Radix Sort function */
 void radix_sort(int *array, size_t size) {
-    int max;
-    int exp;
+	int max;
+	int exp;
 
-    if (size < 2) {
-        return; /* Array already sorted */
-    }
+	if (size < 2) {
+		return; /* Array already sorted */
+	}
 
-    max = getMax(array, size); /* Moved declaration to the beginning */
-    for (exp = 1; max / exp > 0; exp *= 10) {
-        countSort(array, size, exp);
-        print_array(array, size);
-    }
+	max = getMax(array, size); /* Moved declaration to the beginning */
+	for (exp = 1; max / exp > 0; exp *= 10) {
+		countSort(array, size, exp);
+		print_array(array, size);
+	}
 }
