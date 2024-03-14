8354a3a926c6c502df886b84663ee5750c5f9248
Rbrogniez <5013@holbertonschool.com>
2024-03-14 11:46:24 +0100

    update radix sort

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -4,7 +4,8 @@
 /* Helper function to get the maximum value in the array */
 int getMax(int *array, size_t size) {
     int max = array[0];
-    for (size_t i = 1; i < size; i++) {
+    size_t i;
+    for (i = 1; i < size; i++) {
         if (array[i] > max) {
             max = array[i];
         }
@@ -20,21 +21,22 @@ void countSort(int *array, size_t size, int exp) {
         return;
     }
     int count[10] = {0};
+    size_t i;
 
-    for (size_t i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         count[(array[i] / exp) % 10]++;
     }
 
-    for (int i = 1; i < 10; i++) {
-        count[i] += count[i - 1];
+    for (int j = 1; j < 10; j++) {
+        count[j] += count[j - 1];
     }
 
-    for (int i = size - 1; i >= 0; i--) {
-        output[count[(array[i] / exp) % 10] - 1] = array[i];
-        count[(array[i] / exp) % 10]--;
+    for (i = size; i > 0; i--) {
+        output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
+        count[(array[i - 1] / exp) % 10]--;
     }
 
-    for (size_t i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         array[i] = output[i];
     }
 
@@ -48,8 +50,8 @@ void radix_sort(int *array, size_t size) {
     }
 
     int max = getMax(array, size);
-
-    for (int exp = 1; max / exp > 0; exp *= 10) {
+    int exp;
+    for (exp = 1; max / exp > 0; exp *= 10) {
         countSort(array, size, exp);
         print_array(array, size);
     }
