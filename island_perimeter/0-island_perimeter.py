bd462c1687db7613d286ab5764b58f64d8c9fda7
Rbrogniez <5013@holbertonschool.com>
2024-03-19 09:30:43 +0100

    island perimeter

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+def island_perimeter(grid):
+    perimeter = 0
+
+    # Check each cell in the grid
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:  # If the cell represents land
+                # Check each side of the cell
+                # If the side is water or outside the grid, add 1 to the perimeter
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
